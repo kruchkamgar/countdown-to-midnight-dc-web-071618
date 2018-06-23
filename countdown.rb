c56967d0ec79f44f854a9364746b88ca039381fe
kruchkamgar <kruchkamgar@gmail.com>
2018-06-23 17:28:05 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown (count_down)
+
+  count_down.times do
+    puts "#{count_down} SECOND(S)!"
+    count_down -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (count_down)
+  count_down.times do
+    puts "#{count_down} SECOND(S)!"
+    count_down -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
